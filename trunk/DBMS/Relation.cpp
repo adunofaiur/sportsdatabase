80ceeb094ceb865ab01d7f2b6d4c34ca48b70bcc
PaulygontheMathguy@gmail.com <PaulygontheMathguy@gmail.com@7735905e-702c-3a40-c826-38ae119d455d>
2013-09-05 15:04:31 +0000

    Added constructor and Data structure.

--- trunk/DBMS/Relation.cpp
+++ trunk/DBMS/Relation.cpp
@@ -3,7 +3,22 @@
 
 class Relation{
   private://Member data goes here
-  
+    
+    string name;
+    string relative1, relative2;//These will be entity/relation type names.  For instance, isSponsor would relate COMPANY and TEAM
+    vector< vector < string> > Data;
+    
   public://Functions go here
+  
+  Relation(string rel_name, string rel1, string rel2){
+    name = rel_name;
+    relative1 = rel1;
+    relative2 = rel2;
+    Data.push_back(new vector<string>)
+    Data[0].push_back(relname);
+    Data[0].push_back(rel1);
+    Data[0].push_back(rel2);
+
+  }
   
 }
