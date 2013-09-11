96175c0129887893d6c93cf1425a5e0a7425bee4
jonathansandell <jonathansandell@gmail.com>
2013-09-11 02:14:53 +0000

    Token class

--- Parser/Token.cpp
+++ Parser/Token.cpp
@@ -1,2 +1,11 @@
-//Token.h
+//Token.cpp
 //Defines Token Class
+
+struct Token{
+	char kind;
+	char value;
+
+	Token() : kind(' '), value(' '){}  
+	Token(char k) : kind(k), value(' '){}
+	Token(char k, char v) : kind(k), value(v){}
+};
