9e5663d0a9067aa06650459eca86e1d83e0cee1a
matthew21573 <matthew21573@gmail.com>
2013-09-14 18:07:05 +0000

    Added a space adding function

--- Parser/Lexer.cpp
+++ Parser/Lexer.cpp
@@ -1,2 +1,24 @@
 //Lexer.cpp
 //Defines Lexer.  Take Plain-text input, convert to token stream
+
+//This function adds spaces to delineate future tokens
+
+#include <string>
+string add_spaces(string s){
+    for (auto i = s.begin(); i != s.end()-1; i++){
+   
+        if (*i == '"' || *i == '(' || *i == ')' || *i == ';' || ','){
+            if (*(i+1) != ' '){
+                s.insert((i+1), ' ');
+            }
+            if (i != s.begin()){
+                if (*(i-1) != ' '){
+                    s.insert(i, ' ');
+                }
+            }
+        }
+        
+        
+    }
+    return s;
+}
