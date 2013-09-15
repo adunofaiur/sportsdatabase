3f42e583e5e3055a72281dc5a706735ca26b3c11
matthew21573@gmail.com <matthew21573@gmail.com@7735905e-702c-3a40-c826-38ae119d455d>
2013-09-15 00:32:07 +0000

    header for token class

---
+++ trunk/Parser/Token.h
@@ -0,0 +1,29 @@
+//
+//  Token.h
+//  delimeter_inserter
+//
+//  Created by Matthew Carrasco on 9/14/13.
+//  Copyright (c) 2013 Matthew Carrasco. All rights reserved.
+//
+
+#ifndef __delimeter_inserter__Token__
+#define __delimeter_inserter__Token__
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Token{
+private:
+    char type;
+    string val;
+public:
+    Token(string new_val);
+    Token(char new_type, string new_val);
+    
+    char get_id() { return type; }
+    string get_cal() {return val;}
+    
+};
+
+#endif /* defined(__delimeter_inserter__Token__) */
