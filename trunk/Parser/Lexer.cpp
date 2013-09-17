778dfb254c500aae6ffcd471fe2943b81aea0cdb
matthew21573@gmail.com <matthew21573@gmail.com@7735905e-702c-3a40-c826-38ae119d455d>
2013-09-17 00:58:01 +0000

    I'm calling this version the compiling beta

--- trunk/Parser/Lexer.cpp
+++ trunk/Parser/Lexer.cpp
@@ -9,12 +9,21 @@
 #include <algorithm>
 #include <iterator>
 #include "Token.h"
+#include "Token.cpp"
 #include "Parser.cpp"
 using namespace std;
+
+
+
+
+
+
+
+
 //Adds spaces before/after parenthesis, quotes, commas, and semicolons
 string add_spaces(string s){
-    for (auto i = s.begin(); i != s.end()-1; i++){
-   
+    for (string::iterator i = s.begin(); i != s.end()-1; i++){
+        
         if (*i == '(' || *i == ')' || *i == ';' || *i == ','){
             if (*(i+1) != ' '){
                 s.insert((i+1), ' ');
@@ -37,7 +46,7 @@ Token make_token(string s){
     //Check to see if the string is a single special character
     //that is NOT an operand
     if (s == "(" || s == ")" || s == ";" || s == "," || "<-"){
-        
+        Token t = Token ('a', s);
         return Token('h', s);
     }
     //Checks to see if the string is an operator
@@ -45,17 +54,17 @@ Token make_token(string s){
         return Token('o', s);
     }
     else if (s == "+" || s == "-" || s == "*" || s == "rename" ||
-        s == "project" || s == "select"){
+             s == "project" || s == "select"){
         return Token('e', s);
     }
     else if (s == "DELETE" || s == "VARCHAR" || s == "INTEGER" || s == "DELETE" || s== "INSERT" || s == "INTO" || s == "UPDATE" || s == "OPEN" || s == "CLOSE" || s == "WRITE" ||
-        s == "EXIT" || s == "SHOW" || s == "CREATE" || s  == "VALUES" || s == "FROM" ||
-        s == "RELATION" || s == "WHERE"){
+             s == "EXIT" || s == "SHOW" || s == "CREATE" || s  == "VALUES" || s == "FROM" ||
+             s == "RELATION" || s == "WHERE"){
         return Token('c', s);
     }
     else {
-        auto a = s.begin();
-        auto b = s.end();
+        string::iterator a = s.begin();
+        string::iterator b = s.end();
         
         if (*a == '"' || *b == '"'){
             s.erase(a);
@@ -76,25 +85,23 @@ vector<Token> tokenize(string s){
     //Splits the string into a vector of strings deliniteated by spaces
     istringstream iss(s);
     copy(istream_iterator<string>(iss),
-                        istream_iterator<string>(),
+         istream_iterator<string>(),
          back_inserter<vector<string> >(s2));
     /*
-    for (auto i = 0; i < s2.size(); i++){
-        cout << s2[i] << "\n";
-    }*/
-    for (auto i = 0; i < s2.size(); i++){
+     for (auto i = 0; i < s2.size(); i++){
+     cout << s2[i] << "\n";
+     }*/
+    for (int i = 0; i < s2.size(); i++){
         t.push_back(make_token(s2[i]));
     }
     return t;
 }
-
-int main(){
-	while(true){
-		string input;
-		cout<<"Please enter a string to evaluate, or hit Control-C to exit.\n";
-		cin>>input;
-		vector<Token> toks = tokenize(input);
-		bool correct = Parse(toks);
-		cout<<"Correctness of input: "<<correct<<endl;
-	}
+int main() {
+    string input;
+    cout << "Enter strings to evaluate correctness: ";
+    getline(cin, input);
+        vector<Token> toks = tokenize(input);
+        bool correct = Parse(toks);
+        cout << "Is correct?: " << correct << endl;
+    return 0;
 }
